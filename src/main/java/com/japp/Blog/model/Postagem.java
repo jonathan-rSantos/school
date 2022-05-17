38f3ac1a2e0a5fdfa63bdca2f5ee6fb7e3f5c3b4
Jonathan <jonathan.ribeiro28121997@gmail.com>
2022-05-17 20:58:57 -0300

    Entity, Controller and Repository tema

--- src/main/java/com/japp/Blog/model/Postagem.java
+++ src/main/java/com/japp/Blog/model/Postagem.java
@@ -6,18 +6,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "postagem")
 public class Postagem {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@NotNull
 	@Size(min = 5, max = 100)
@@ -27,6 +30,17 @@ public class Postagem {
 	private String texto;
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public long getId() {
 		return id;
---
+++ src/main/java/com/japp/Blog/repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package com.japp.Blog.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.japp.Blog.model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
